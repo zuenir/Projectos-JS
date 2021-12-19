e82b6bed21ea3c9f0e44a872fd975bf9b448a1f5
zuenir <zuenirlima@gmail.com>
2021-12-19 16:17:25 -0300

    first commit

---
+++ urna/etapas.js
@@ -0,0 +1,45 @@
+let etapas = [
+  {
+    titulo: "VEREADOR",
+    numeros: 5,
+    candidatos: [
+      {
+        numero: 38111,
+        nome: "Fulano de Tal",
+        partido: 'ABC',
+        foto: [{ url: "38111.png", legenda: "Vereador", small: false }],
+      },
+      {
+        numero: 77222,
+        name: "Zuenir Claudio",
+        partido: 'DEFG',
+        fotos: [{ url: "77222.png", legenda: "Vereador"}],
+      }
+    ]
+  },
+  {
+    titulo: "PREFEITO",
+    numeros: 2,
+    candidatos: [
+      {
+        numero: 99,
+        nome: "Ciclano",
+        partido: 'ABC',
+        vice: 'Cic',
+        foto: [
+            { url: "99.png", legenda: "Prefieto" },
+            { url: "99_2.png", legenda: "Vice-Prefeito", small: false }
+        ],
+      },
+      {
+        numero: 84,
+        name: "Claudio",
+        partido: 'Zul',
+        fotos: [
+            { url: "84.png", legenda: "Prefeito"},
+            { url: "84_2.jpg", legenda: "Vice-Prefeito",small: true}
+        ],
+      }
+    ]
+  },
+];
